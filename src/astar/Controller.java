a398309c2ea81f9c71df8f35ef5676c1491af40a
andy-fitz <andrew.fitzgerald@ucdconnect.ie>
2018-05-27 20:24:27 +0100

    Adding neighbours to each Node.

--- src/astar/Controller.java
+++ src/astar/Controller.java
@@ -10,9 +10,10 @@ public class Controller {
 
     private double width = 900;
     private double height = 900;
+    private int rows = 45;
+    private int cols = 45;
 
-    private Node[][] grid = new Node[45][45];
-
+    private Node[][] grid = new Node[rows][cols];
 
     @FXML
     Pane environment;
@@ -24,14 +25,18 @@ public class Controller {
         environment.getChildren().add(canvas);
         draw();
         drawNodes();
+        drawPath(grid[20][25], grid[44][30]);
+        System.out.println(grid.length);
     }
 
+    // Draw Background.
     public void draw(){
         GraphicsContext g = canvas.getGraphicsContext2D();
         g.setFill(Color.GREEN);
         g.fillRect(0,0,width,height);
     }
 
+    // Draw Node Grid.
     public void drawNodes(){
         GraphicsContext g = canvas.getGraphicsContext2D();
         g.setFill(Color.WHITE);
@@ -39,12 +44,24 @@ public class Controller {
         for(int i=0; i<width; i+=20){
             for(int j=0; j<height; j+=20){
                 g.fillOval(i+5,j+5,10,10);
-                grid[i/20][j/20] = new Node(i+10,j+10);
-                System.out.println(grid[i/20][j/20].toString());
+                grid[i/20][j/20] = new Node(i+10,j+10, i/20, j/20);
             }
         }
 
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                grid[i][j].addNeighbours(grid);
+            }
+        }
+    }
 
+    // Draw a line between two Nodes.
+    public void drawPath(Node node1, Node node2){
+        GraphicsContext g = canvas.getGraphicsContext2D();
+        g.setFill(Color.ORANGE);
+        g.setStroke(Color.ORANGE);
+        g.setLineWidth(5);
+        g.strokeLine(node1.getX(),node1.getY(),node2.getX(),node2.getY());
     }
 
 
--- src/astar/Node.java
+++ src/astar/Node.java
@@ -4,13 +4,20 @@ import java.util.ArrayList;
 
 public class Node {
 
+    private int i;
+    private int j;
     private double x;
     private double y;
-    private ArrayList neighbours;
+    private double f;
+    private double g;
 
-    public Node(double x,double y){
+    private ArrayList<Node> neighbours = new ArrayList<>();
+
+    public Node(double x,double y, int i, int j){
         this.x = x;
         this.y = y;
+        this.i = i;
+        this.j = j;
     }
 
     @Override
@@ -18,7 +25,43 @@ public class Node {
         return "Node: " + x + " " + y;
     }
 
-    public void addNeighbours(){
+    public void addNeighbours(Node[][] grid){
+        System.out.println(i + " " + j);
+        if(i < grid.length-1){
+            neighbours.add(grid[i+1][j]); // Right
+        }
+        if(i > 0) {
+            neighbours.add(grid[i - 1][j]); // Left
+        }
+        if (j < grid.length -1) {
+            neighbours.add(grid[i][j + 1]); // Bottom
+        }
+        if(j > 0) {
+            neighbours.add(grid[i][j - 1]); // Top
+        }
+    }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public void setF(double f) {
+        this.f = f;
+    }
+
+    public double getG() {
+        return g;
+    }
+
+    public void setG(double g) {
+        this.g = g;
     }
 }
